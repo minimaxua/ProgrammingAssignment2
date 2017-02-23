1a827ab05da699431981d602c58629f02af8a5b0
minimaxua <minimaxua@users.noreply.github.com>
2017-02-23 12:26:20 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## Assume that the matrix supplied is always invertible.
+## In the real world you may meet with  - "Error in solve.default(data, ...) : 
+## Lapack routine dgesv: system is exactly singular:Lapack routine dgesv:  ...
+## system is exactly singular"
+## Just try another matrix (with det<>0)
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inverse) inverse <<- inverse
+        getinverse <- function() inverse
+        
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+## cacheSolve technically makes inverse or gets cashed data
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
+        
+## Return a matrix that is the inverse of 'x' or calculate and return
 }
